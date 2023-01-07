eb55d1f9f35913381342d6848230e99e11b6e45b
B-DeshiDev <ffalsoy@gmail.com>
2023-01-07 14:20:56 +0600

    Level 1 tutorial added

---
+++ Assets/Scripts/Tutorial/JumpTutorial.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Core.Input;
+using Core.Misc;
+using UnityEngine;
+
+namespace Tutorial
+{
+    public class JumpTutorial: MonoBehaviour
+    {
+        public GameObject jumpInstructionsText;
+        public GameObject jumpHintText;
+
+        private bool playerInRange = false;
+
+        private void Start()
+        {
+            InputManager.jumpButton.addPerformedCallback(gameObject, handleJumpPressed);
+        }
+
+        private void handleJumpPressed()
+        {
+            if (playerInRange)
+            {
+                jumpInstructionsText.SetActive(false);
+                jumpHintText.SetActive(true);
+            }
+        }
+
+        public void handlePlayerEntered()
+        {
+            playerInRange = true;
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.CompareTag(SceneVarTracker.PlayerTag))
+            {
+                handlePlayerEntered();
+            }
+        }
+    }
+}
